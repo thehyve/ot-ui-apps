e80896ae00df098557b6881ab1d19a04fbd1d0da
Fedde Schaeffer <fedde@thehyve.nl>
2022-10-26 18:04:58 +0200

    Shadow the top bar to hint at content scrolling up
    As documented under <https://mui.com/material-ui/react-app-bar/#elevate-app-bar>

--- apps/platform/src/components/TopBar.jsx
+++ apps/platform/src/components/TopBar.jsx
@@ -1,52 +1,67 @@
 import React from 'react';
 
+import AppBar from '@material-ui/core/AppBar';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+
+function ElevationScroll({children}) {
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
 export default function TopBar() {
   return (
     <>
       {/* the first div will push the navigation bar down, which is otherwise
-	  hidden behind the company bar */}
+          hidden behind the company bar */}
       <div id="placeholder-div" style={{ height: '50px', width: '100%' }} />
-      <div
-        id="ot-org-top-bar"
-        style={{
-          position: 'fixed',
-          backgroundColor: '#005284',
-          zIndex: 2000000,
-          boxShadow: '0 4px 5px 0px grey',
-          height: '50px',
-          width: '100%',
-        }}
-      >
-        <img
-          src="/assets/img/logo-org.png"
-          title="Logo"
-          alt="Logo"
+      <ElevationScroll>
+        <AppBar
+          id="ot-org-top-bar"
           style={{
-            float: 'left',
-            marginLeft: '20px',
-            marginTop: '5px',
-            height: '40px',
-          }}
-        />
-        {/* TODO: enable this part through configuration?
-	          not all deployments use the top bar _and_ Keycloak
-		  however, this will be invisible if no user info available
-	*/}
-        <span
-          id="ot-org-username"
-          title="Logged in user"
-          style={{
-            float: 'right',
-            marginRight: '20px',
-            marginTop: '15px',
-            color: 'white',
-            fontFamily: 'Inter, serif',
-            fontStyle: 'italic',
+            display: 'block',
+            position: 'fixed',
+            backgroundColor: '#005284',
+            zIndex: 2000000,
+            height: '50px',
+            width: '100%',
           }}
         >
-          &nbsp;
-        </span>
-      </div>
+          <img
+            src="/assets/img/logo-org.png"
+            title="Logo"
+            alt="Logo"
+            style={{
+              float: 'left',
+              marginLeft: '20px',
+              marginTop: '5px',
+              height: '40px',
+            }}
+          />
+          {/* TODO: enable this part through configuration?
+                    not all deployments use the top bar _and_ Keycloak
+                    however, this will be invisible if no user info available
+          */}
+          <span
+            id="ot-org-username"
+            title="Logged in user"
+            style={{
+              float: 'right',
+              marginRight: '20px',
+              marginTop: '15px',
+              color: 'white',
+              fontFamily: 'Inter, serif',
+              fontStyle: 'italic',
+            }}
+          >
+            &nbsp;
+          </span>
+        </AppBar>
+      </ElevationScroll>
     </>
   );
 }
