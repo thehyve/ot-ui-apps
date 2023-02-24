adb789a3f8bc565918c6bbe5c6fdaacbf6f1d2a9
Roman Hillje <roman.hillje@googlemail.com>
2023-02-24 15:30:18 +0100

    Add option to show top bar with logo and username
    Keeping other elements from disappearing behind it or appearing in front
    of it.
    
    Co-authored-by: Fedde Schaeffer <fedde@thehyve.nl>
    Co-authored-by: Alessia Peviani <alessia@thehyve.nl>

--- apps/platform/src/components/AssociationsToolkit/components/Table/TableHeader.jsx
+++ apps/platform/src/components/AssociationsToolkit/components/Table/TableHeader.jsx
@@ -1,22 +1,28 @@
-import { useState } from "react";
-import { flexRender } from "@tanstack/react-table";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowDownWideShort, faBook, faLock } from "@fortawesome/free-solid-svg-icons";
-import { Grid } from "@mui/material";
+import { useState } from 'react';
+import { flexRender } from '@tanstack/react-table';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faArrowDownWideShort,
+  faBook,
+  faLock,
+} from '@fortawesome/free-solid-svg-icons';
+import { Grid } from '@mui/material';
 
-import AggregationsRow from "./AggregationsRow";
-import useAotfContext from "../../hooks/useAotfContext";
-import { GridContainer } from "../layout";
+import AggregationsRow from './AggregationsRow';
+import useAotfContext from '../../hooks/useAotfContext';
+import { GridContainer } from '../layout';
+import classNames from 'classnames';
+import { showTopBar } from '../../utils';
 
 const getHeaderContainerClassName = ({ id }) => {
-  if (id === "1_naiming-cols_name") return "naiming-cols";
-  return "entity-cols";
+  if (id === '1_naiming-cols_name') return 'naiming-cols';
+  return 'entity-cols';
 };
 
 const getHeaderClassName = ({ id }) => {
-  if (id === "name") return "header-name";
-  if (id === "score") return "rotate header-score";
-  return "rotate";
+  if (id === 'name') return 'header-name';
+  if (id === 'score') return 'rotate header-score';
+  return 'rotate';
 };
 
 function TableHeader({ table, cols }) {
@@ -29,20 +35,25 @@ function TableHeader({ table, cols }) {
   } = useAotfContext();
   const [activeAggregation, setActiveAggegation] = useState(null);
   const onEnterHoverHeader = ({ id: elementId, column }) => {
-    if (elementId === "score" || elementId === "name") return;
+    if (elementId === 'score' || elementId === 'name') return;
     const { aggregation } = column.columnDef;
     setActiveAggegation(aggregation);
   };
 
   const onLeaveHoverHeader = () => {
-    if (id === "score" || id === "name") return;
+    if (id === 'score' || id === 'name') return;
     setActiveAggegation(null);
   };
 
   const highLevelHeaders = table.getHeaderGroups()[0].headers;
 
   return (
-    <div className="Theader">
+    <div
+      className={classNames({
+        Theader: true,
+        'avoiding-top-bar': showTopBar,
+      })}
+    >
       <Grid container direction="row" wrap="nowrap">
         {highLevelHeaders.map(highLevelHeader => (
           <GridContainer
@@ -55,7 +66,9 @@ function TableHeader({ table, cols }) {
                 {header.isPlaceholder ? null : (
                   <div
                     {...{
-                      className: header.column.getCanSort() ? "cursor-pointer select-none" : "",
+                      className: header.column.getCanSort()
+                        ? 'cursor-pointer select-none'
+                        : '',
                     }}
                     onMouseEnter={() => onEnterHoverHeader(header)}
                     onMouseLeave={() => onLeaveHoverHeader()}
@@ -65,14 +78,23 @@ function TableHeader({ table, cols }) {
                         onClick: header.column.getToggleSortingHandler(),
                       }}
                     >
-                      {flexRender(header.column.columnDef.header, header.getContext())}
+                      {flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
                     </div>
                     {header.column.columnDef.isPrivate && (
-                      <FontAwesomeIcon className="header-desc-icon" icon={faLock} />
+                      <FontAwesomeIcon
+                        className="header-desc-icon"
+                        icon={faLock}
+                      />
                     )}
                     {{
                       desc: (
-                        <FontAwesomeIcon className="header-desc-icon" icon={faArrowDownWideShort} />
+                        <FontAwesomeIcon
+                          className="header-desc-icon"
+                          icon={faArrowDownWideShort}
+                        />
                       ),
                     }[header.column.getIsSorted()] ?? null}
 
@@ -83,7 +105,10 @@ function TableHeader({ table, cols }) {
                         className="docs-link"
                         href={header.column.columnDef.docsLink}
                       >
-                        <FontAwesomeIcon className="header-desc-icon" icon={faBook} />
+                        <FontAwesomeIcon
+                          className="header-desc-icon"
+                          icon={faBook}
+                        />
                       </a>
                     )}
                   </div>
--- packages/ui/src/components/NavBar.jsx
+++ packages/ui/src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import { Link as ReactRouterLink } from "react-router-dom";
+import { Link as ReactRouterLink } from 'react-router-dom';
 import {
   AppBar,
   Toolbar,
@@ -7,17 +7,18 @@ import {
   MenuItem,
   MenuList,
   useMediaQuery,
-  Box,
-} from "@mui/material";
-import { makeStyles, useTheme } from "@mui/styles";
-import { styled } from "@mui/material/styles";
-import classNames from "classnames";
-import { v1 } from "uuid";
+} from '@mui/material';
+import { makeStyles, useTheme } from '@mui/styles';
+import { styled } from '@mui/material/styles';
+import classNames from 'classnames';
+import { v1 } from 'uuid';
 
-import Link from "./Link";
-import OpenTargetsTitle from "./OpenTargetsTitle";
-import HeaderMenu from "./HeaderMenu";
-import PrivateWrapper from "./PrivateWrapper";
+import Link from './Link';
+import OpenTargetsTitle from './OpenTargetsTitle';
+import HeaderMenu from './HeaderMenu';
+import PrivateWrapper from './PrivateWrapper';
+import config from '../config';
+import { TopBar } from 'ui';
 
 const LogoBTN = styled(Button)`
   border: none;
@@ -28,76 +29,110 @@ const useStyles = makeStyles(theme => ({
   navbar: {
     backgroundColor: `${theme.palette.primary.main} !important`,
     margin: 0,
-    width: "100%",
+    width: '100%',
   },
   navbarHomepage: {
     left: 0,
     top: 0,
-    position: "absolute !important",
+    position: 'absolute !important',
   },
   flex: {
     flexGrow: 1,
   },
   menuExternalLinkContainer: {
-    fontSize: "1rem",
-    "&:first-of-type": {
-      marginLeft: "1rem",
+    fontSize: '1rem',
+    '&:first-of-type': {
+      marginLeft: '1rem',
     },
-    "&:not(:last-child)": {
-      marginRight: "1rem",
+    '&:not(:last-child)': {
+      marginRight: '1rem',
     },
   },
   menuExternalLink: {
-    color: "inherit",
-    textDecoration: "none",
-    "&:hover": {
+    color: 'inherit',
+    textDecoration: 'none',
+    '&:hover': {
       color: theme.palette.secondary.main,
     },
   },
   menuList: {
-    display: "flex",
+    display: 'flex',
   },
   menuLink: {
     color: theme.palette.secondary.contrastText,
-    margin: `0 ${theme.spacing(2)}`,
-    whiteSpace: "nowrap",
-    "&:hover": {
+    '&:hover': {
       color: theme.palette.secondary.contrastText,
     },
   },
   spaceBetween: {
-    display: "flex",
-    justifyContent: "space-between",
+    display: 'flex',
+    justifyContent: 'space-between',
   },
   navLogo: {
     flex: 1,
   },
   navSearch: {
     flex: 1,
-    display: "flex",
-    justifyContent: "center",
+    display: 'flex',
+    justifyContent: 'center',
   },
   navMenu: {
     flex: 1,
-    display: "flex",
-    justifyContent: "end",
+    display: 'flex',
+    justifyContent: 'end',
   },
 }));
 
 function MenuExternalLink({ classes, href, children }) {
   return (
     <Typography color="inherit" className={classes.menuExternalLinkContainer}>
-      <a target="_blank" rel="noopener noreferrer" href={href} className={classes.menuExternalLink}>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href={href}
+        className={classes.menuExternalLink}
+      >
         {children}
       </a>
     </Typography>
   );
 }
 
-function NavBar({ name, search, api, downloads, docs, contact, homepage, items, placement }) {
+function NavBar(props) {
+  return (
+    <>
+      {/*
+       * Keep the TopBar outside of the NavBar's AppBar component, as nesting it
+       * renders the top bar behind the ProtVista protein structure viewer when
+       * scrolling down the OTP target profile page. That's probably because the
+       * NavBar's AppBar has its own z-index lower than 40001, which creates a
+       * local stacking context outside of which the z-indices of descendants
+       * are not compared.
+       *
+       * This still leaves the issue that the bar also overlays the 3d structure
+       * viewer when it's expanded to fill the viewport, blocking some of the
+       * buttons of the viewer.
+       */}
+      {config.showTopBar && <TopBar />}
+      <NavBarContent {...props} />
+    </>
+  );
+}
+
+function NavBarContent({
+  name,
+  search,
+  api,
+  downloads,
+  docs,
+  contact,
+  homepage,
+  items,
+  placement,
+}) {
   const classes = useStyles();
   const theme = useTheme();
-  const smMQ = useMediaQuery(theme.breakpoints.down("sm"));
+  const smMQ = useMediaQuery(theme.breakpoints.down('sm'));
   const isHomePageRegular = homepage && !smMQ;
   return (
     <AppBar
@@ -108,6 +143,10 @@ function NavBar({ name, search, api, downloads, docs, contact, homepage, items, 
       color="primary"
       elevation={0}
     >
+      {/* push the content down so it isn't hidden behind the logo bar */}
+      {config.showTopBar && (
+        <div id="placeholder-div" style={{ height: "50px", width: "100%" }} />
+      )}
       <Toolbar variant="dense" className={classNames(classes.spaceBetween)}>
         <div className={classes.navLogo}>
           {homepage ? null : (
@@ -144,38 +183,41 @@ function NavBar({ name, search, api, downloads, docs, contact, homepage, items, 
             </MenuExternalLink>
           ) : null}
 
-          {items && !isHomePageRegular ? <HeaderMenu items={items} placement={placement} /> : null}
+          {items && !isHomePageRegular ? (
+            <HeaderMenu items={items} placement={placement} />
+          ) : null}
 
           {isHomePageRegular && (
-            <Box sx={{ display: "flex" }}>
+            <MenuList className={classes.menuList}>
               {items.map(item => {
                 if (item.showOnlyPartner) {
                   return (
                     <PrivateWrapper key={v1()}>
-                      <Link
-                        footer
-                        external={item.external}
-                        to={item.url}
-                        className={classes.menuLink}
-                      >
-                        <Typography variant="body2">{item.name}</Typography>
-                      </Link>
+                      <MenuItem dense className={classes.menuItem}>
+                        <Link
+                          external={item.external}
+                          to={item.url}
+                          className={classes.menuLink}
+                        >
+                          {item.name}
+                        </Link>
+                      </MenuItem>
                     </PrivateWrapper>
                   );
                 }
                 return (
-                  <Link
-                    key={v1()}
-                    footer
-                    external={item.external}
-                    to={item.url}
-                    className={classes.menuLink}
-                  >
-                    <Typography variant="body2">{item.name}</Typography>
-                  </Link>
+                  <MenuItem key={v1()} dense className={classes.menuItem}>
+                    <Link
+                      external={item.external}
+                      to={item.url}
+                      className={classes.menuLink}
+                    >
+                      {item.name}
+                    </Link>
+                  </MenuItem>
                 );
               })}
-            </Box>
+            </MenuList>
           )}
         </div>
       </Toolbar>
