aa38f9fcbfb846657fcf299e06c65ad970885ddb
Roman Hillje <roman.hillje@googlemail.com>
2023-02-24 15:30:18 +0100

    Add option to show top bar with logo and username
    Keeping other elements from disappearing behind it or appearing in front
    of it.
    
    Co-authored-by: Fedde Schaeffer <fedde@thehyve.nl>
    Co-authored-by: Alessia Peviani <alessia@thehyve.nl>

--- packages/ui/src/components/NavBar.tsx
+++ packages/ui/src/components/NavBar.tsx
@@ -11,6 +11,8 @@ import Link from "./Link";
 import OpenTargetsTitle from "./OpenTargetsTitle";
 import HeaderMenu from "./HeaderMenu";
 import PrivateWrapper from "./PrivateWrapper";
+import { useConfigContext } from "../providers/ConfigurationProvider";
+import { TopBar } from "ui";
 
 const LogoBTN = styled(Button)<{ component?: React.ElementType; to?: string }>`
   border: none;
@@ -113,7 +115,33 @@ function MenuExternalLink({ classes, href, children }: MenuExternalLinkProps): R
   );
 }
 
-function NavBar({ name, search, api, downloads, docs, contact, homepage, items, placement }: NavBarProps): ReactElement {
+function NavBar(props: NavBarProps): ReactElement {
+  const {
+    config: { showTopBar },
+  } = useConfigContext();
+  return (
+    <>
+      {/*
+       * Keep the TopBar outside of the NavBar's AppBar component, as nesting it
+       * renders the top bar behind the ProtVista protein structure viewer when
+       * scrolling down the OTP target profile page. That's probably because the
+       * NavBar's AppBar has its own z-index lower than 40001, which creates a
+       * local stacking context outside of which the z-indices of descendants
+       * are not compared.
+       *
+       * This still leaves the issue that the bar also overlays the 3d structure
+       * viewer when it's expanded to fill the viewport, blocking some of the
+       * buttons of the viewer.
+       */}
+      {showTopBar && <TopBar />}
+      <NavBarContent {...props} showTopBar={showTopBar} />
+    </>
+  );
+}
+
+type NavBarContentProps = NavBarProps & { showTopBar: boolean };
+
+function NavBarContent({ name, search, api, downloads, docs, contact, homepage, items, placement, showTopBar }: NavBarContentProps): ReactElement {
   const classes = useStyles();
   const theme = useTheme<Theme>();
   const smMQ = useMediaQuery(theme.breakpoints.down("sm"));
@@ -127,6 +155,8 @@ function NavBar({ name, search, api, downloads, docs, contact, homepage, items, 
       color="primary"
       elevation={0}
     >
+      {/* push the content down so it isn't hidden behind the logo bar */}
+      {showTopBar && <div id="placeholder-div" style={{ height: "50px", width: "100%" }} />}
       <Toolbar variant="dense" className={classNames(classes.spaceBetween)}>
         {homepage ? null : (
           <Box
