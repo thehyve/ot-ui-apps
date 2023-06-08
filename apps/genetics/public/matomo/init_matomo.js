19ba91fa53744b692304bac9b1b215be28e31a99
Alessia Peviani <alessia@thehyve.nl>
2023-06-08 18:18:23 +0200

    Add matomo tracking

---
+++ apps/genetics/public/matomo/init_matomo.js
@@ -0,0 +1,37 @@
+var _paq = window._paq || [];
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+
+(function() {
+  function registerUserVisit() {
+    getLoggedInUser()
+      .then(function(user) {
+        _paq.push(['setUserId', user]);
+      })
+      .finally(function() {
+        _paq.push(['setCustomUrl', window.location.href]);
+        _paq.push(['setDocumentTitle', window.document.title]);
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+      });
+  }
+  var pushState = history.pushState;
+  history.pushState = function() {
+    pushState.apply(history, arguments);
+    registerUserVisit();
+  };
+  registerUserVisit();
+})();
+
+(function() {
+  var u = 'localhost:9443'; // '/matomo/';
+  _paq.push(['setTrackerUrl', u + 'matomo.php']);
+  _paq.push(['setSiteId', '1']);
+  var d = document,
+      g = d.createElement('script'),
+      s = d.getElementsByTagName('script')[0];
+  g.type = 'text/javascript';
+  g.async = true;
+  g.defer = true;
+  g.src = u + 'matomo.js';
+  s.parentNode.insertBefore(g, s);
+})();
