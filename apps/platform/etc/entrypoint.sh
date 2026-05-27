df2dd1e0622b2f93d3074928d8354101c5066198
Robin Meijer <76998782+RobinM-code@users.noreply.github.com>
2026-05-27 15:37:39 +0200

    Update docker (#37)

--- apps/platform/etc/entrypoint.sh
+++ apps/platform/etc/entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 cat << EOF > /usr/share/nginx/html/config.js
+var gitVersion = '${WEBAPP_GIT_VERSION:-}'
 var configUrlApi = '${WEBAPP_API_URL:-https://api.platform.opentargets.org/api/v4/graphql}'
 var configOTAiApi = '${WEBAPP_OPENAI_URL:-https://ai.platform.opentargets.org}'
 var configGoogleTagManagerID = '${WEBAPP_GOOGLE_TAG_MANAGER_ID:-GTM-XXXXX}'
+var configShowTopBar = ${WEBAPP_SHOW_TOP_BAR:-false}
+configProfile['helpdeskEmail'] = '${WEBAPP_HELPDESK_EMAIL}'
 EOF
 
 cat "/usr/share/nginx/html/profiles/${WEBAPP_FLAVOR:-platform}.js" >> /usr/share/nginx/html/config.js
